6b5a2a31d0ddcbbe57c6b144129e9391e5ceef6e
Steve Corbett <steve@corbett.ca>
2026-02-01 17:35:35 -0700

    Issue #5 - upgrade to ImageViewer 3

--- src/main/java/ca/corbett/imageviewer/extensions/imageconvert/ImageConverterExtension.java
+++ src/main/java/ca/corbett/imageviewer/extensions/imageconvert/ImageConverterExtension.java
@@ -1,14 +1,15 @@
 package ca.corbett.imageviewer.extensions.imageconvert;
 
 import ca.corbett.extensions.AppExtensionInfo;
+import ca.corbett.extras.EnhancedAction;
+import ca.corbett.extras.io.KeyStrokeManager;
 import ca.corbett.extras.properties.AbstractProperty;
+import ca.corbett.extras.properties.KeyStrokeProperty;
+import ca.corbett.imageviewer.AppConfig;
 import ca.corbett.imageviewer.extensions.ImageViewerExtension;
 import ca.corbett.imageviewer.ui.MainWindow;
+import ca.corbett.imageviewer.ui.ReservedKeyStrokeWorkaround;
 
-import javax.swing.JMenuItem;
-import javax.swing.KeyStroke;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +17,12 @@ import java.util.List;
  * An ImageViewer extension that allows you to convert either a single image or a directory
  * of images from PNG to Jpeg or vice versa.
  *
- * @author scorbo2
+ * @author <a href="https://github.com/scorbo2">scorbo2</a>
  * @since 2023-12-31 - Happy New Year!
  */
 public class ImageConverterExtension extends ImageViewerExtension {
 
+    private static final String keyStrokeProp = AppConfig.KEYSTROKE_PREFIX + "Image Converter.convertKeyStroke";
     private final AppExtensionInfo extInfo;
 
     public ImageConverterExtension() {
@@ -33,6 +35,7 @@ public class ImageConverterExtension extends ImageViewerExtension {
 
     @Override
     public void loadJarResources() {
+        // Nothing to load here.
     }
 
     @Override
@@ -42,38 +45,42 @@ public class ImageConverterExtension extends ImageViewerExtension {
 
     @Override
     protected List<AbstractProperty> createConfigProperties() {
-        return null;
+        List<AbstractProperty> props = new ArrayList<>();
+
+        props.add(new KeyStrokeProperty(keyStrokeProp, "Image converter:",
+                                        KeyStrokeManager.parseKeyStroke("Ctrl+J"),
+                                        new ImageConverterAction())
+                      .setAllowBlank(true)
+                      .addFormFieldGenerationListener(new ReservedKeyStrokeWorkaround()));
+
+        return props;
     }
 
     @Override
-    public List<JMenuItem> getMenuItems(String topLevelMenu, MainWindow.BrowseMode browseMode) {
+    public List<EnhancedAction> getMenuActions(String topLevelMenu, MainWindow.BrowseMode browseMode) {
+        // We don't support image set mode, because image sets can
+        // contain images from many different directories, which would break our dialog.
+        // We COULD consider this as a future feature, but the UI might be very confusing.
         if (browseMode == MainWindow.BrowseMode.IMAGE_SET) {
-            return null; // we COULD allow conversion from within an image set...
+            return null;
         }
 
         if ("Edit".equals(topLevelMenu)) {
-            List<JMenuItem> list = new ArrayList<>();
-            list.add(buildMenuItem());
-            return list;
+            return List.of(new ImageConverterAction());
         }
+
         return null;
     }
 
     @Override
-    public List<JMenuItem> getPopupMenuItems(MainWindow.BrowseMode browseMode) {
+    public List<EnhancedAction> getPopupMenuActions(MainWindow.BrowseMode browseMode) {
+        // We don't support image set mode, because image sets can
+        // contain images from many different directories, which would break our dialog.
+        // We COULD consider this as a future feature, but the UI might be very confusing.
         if (browseMode == MainWindow.BrowseMode.IMAGE_SET) {
-            return null; // we COULD allow conversion from within an image set...
+            return null;
         }
 
-        List<JMenuItem> list = new ArrayList<>();
-        list.add(buildMenuItem());
-        return list;
+        return List.of(new ImageConverterAction());
     }
-
-    private JMenuItem buildMenuItem() {
-        JMenuItem item = new JMenuItem(new ImageConverterAction());
-        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_J, InputEvent.CTRL_DOWN_MASK));
-        return item;
-    }
-
 }
